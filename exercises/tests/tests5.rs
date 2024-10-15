043ce98cf416a04d36962c911139a3b92e58aaf5
Junior <1508651338@qq.com>
2024-10-15 17:31:16 +0800

    10.15

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,8 +22,8 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+use std::mem::MaybeUninit;
 /// # Safety
 ///
 /// The `address` must contain a mutable reference to a valid `u32` value.
@@ -31,8 +31,12 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
+    let mut ptr: *mut u32 = address as *mut u32;
+
+    // 使用 MaybeUninit 来安全地创建一个未初始化的 u32 值
+    let new_value = MaybeUninit::new(0xAABBCCDD);
     unsafe {
-        todo!("Your code goes here")
+        *ptr = new_value.assume_init()
     }
 }
 
